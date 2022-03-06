a0cb8f2aa664498c4dcd3172df38985158490d5e
Julia Silge <julia.silge@gmail.com>
2022-03-06 11:27:14 -0700

    Add example datasets

---
+++ R/data.R
@@ -0,0 +1,62 @@
+#' Working remotely by education
+#'
+#' Time series data on what percent of population are working remotely by
+#' education
+#'
+#' @name cori_education
+#' @aliases cori_education
+#' @docType data
+#' @return \item{cori_education}{a data frame}
+#'
+#' @keywords datasets
+#' @examples
+#' data(cori_education)
+#' str(cori_education)
+NULL
+
+#' CORI network communities
+#'
+#' Communities that are part of the Center for Rural Innovation network
+#'
+#' @name cori_communities
+#' @aliases cori_communities
+#' @docType data
+#' @return \item{cori_communities}{a data frame}
+#'
+#' @keywords datasets
+#' @examples
+#' data(cori_communities)
+#' str(cori_communities)
+NULL
+
+#' Employment in CORI network communities
+#'
+#' Estimates for total population and employment in 2014 and 2019 for ten
+#' CORI communities
+#'
+#' @name cori_employment
+#' @aliases cori_employment
+#' @docType data
+#' @return \item{cori_employment}{a data frame}
+#'
+#' @keywords datasets
+#' @examples
+#' data(cori_employment)
+#' str(cori_employment)
+NULL
+
+#' Poverty in CORI network communities
+#'
+#' Estimates for total population and population below the poverty level in
+#' 2014 and 2019 for ten CORI communities
+#'
+#' @name cori_poverty
+#' @aliases cori_poverty
+#' @docType data
+#' @return \item{cori_poverty}{a data frame}
+#'
+#' @keywords datasets
+#' @examples
+#' data(cori_poverty)
+#' str(cori_poverty)
+NULL
