4e67111bf968679f102552684d5905840eb869b3
Camden Blatchly <camden.blatchly@ruralinnovation.us>
2022-04-11 15:59:13 -0400

    added default plot margin

--- R/theme.R
+++ R/theme.R
@@ -69,6 +69,8 @@ theme_cori <- function(base_family = "Montserrat", base_size = 12) {
     margin = ggplot2::margin(t = 25, b = 0)
   )
 
+  ret$plot.margin <- ggplot2::margin(base_size, base_size, base_size, base_size, "pt"),
+
   # Legend attributes
   ret$legend.background <- ggplot2::element_blank()
   ret$legend.spacing <- ggplot2::unit(20L, "pt")
